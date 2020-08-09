2d2be80e11edd261b613c7ab1dfb50b444798df7
kkirby16 <kkirby16@my.whitworth.edu>
2020-08-08 22:35:23 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,13 @@
 #If it's before noon, greet the user with "Good Morning!"
 #If it's after noon, greet the user with "Good Afternoon!". Remember that getting the time may return results in 24 hour time! Anytime on or after 12 is the afternoon.
 
+require "pry"
+
 class Application
   def call(env)
     resp = Rack::Response.new
-
-    if Time.now.hour >= 12
+    time = Time.now
+    if time.hour >= 12
       resp.write "Good Afternoon!"
     else
       resp.write "Good Morning!"
